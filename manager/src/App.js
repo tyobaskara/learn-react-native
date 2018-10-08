59726dd350a5edad87a900ee594ab6ae71ea8a8c
Prasetya Aji <prasetya.aji@reservasi.com>
2018-10-08 16:33:40 +0700

    firebase

--- manager/src/App.js
+++ manager/src/App.js
@@ -2,9 +2,26 @@ import React, { Component } from 'react';
 import { Platform, StyleSheet, Text, View } from 'react-native';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
+import firebase from '@firebase/app';
+import '@firebase/auth';
 import reducers from './reducers';
 
 class App extends Component {
+  componentWillMount() {
+    // Initialize Firebase
+    const config = {
+      apiKey: 'AIzaSyBrs-5wMmjChlI6rAfVh4ogc35__uIK6YA',
+      authDomain: 'manager-839d6.firebaseapp.com',
+      databaseURL: 'https://manager-839d6.firebaseio.com',
+      projectId: 'manager-839d6',
+      storageBucket: 'manager-839d6.appspot.com',
+      messagingSenderId: '90341411667'
+    };
+
+    firebase.initializeApp(config);
+  }
+  
+
   render() {
     return (
       <Provider store={createStore(reducers)}>
