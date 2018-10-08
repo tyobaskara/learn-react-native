ee1270cf70ce2b5834bcddaede699db51ed3dc28
Prasetya Aji <prasetya.aji@reservasi.com>
2018-10-08 17:39:25 +0700

    password action reducer redux

--- manager/src/components/LoginForm.js
+++ manager/src/components/LoginForm.js
@@ -1,45 +1,52 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { emailChanged } from '../actions';
+import { emailChanged, passwordChanged } from '../actions';
 import { Card, CardSection, Input, Button } from './common';
 
 class LoginForm extends Component {
-    onEmailChange = (text) => {
+    onEmailChange = text => {
         this.props.emailChanged(text);
     }
 
-  render() {
-    return (
-      <Card>
-        <CardSection>
-            <Input 
-                label="Email"
-                placeholder="email@gmail.com"
-                onChangeText={this.onEmailChange}
-                value={this.props.email}
-            />
-        </CardSection>
-        <CardSection>
-            <Input 
-                secureTextEntry
-                label="Password"
-                placeholder="password"
-            />
-        </CardSection>
-        <CardSection>
-            <Button>
-                Login
+    onPasswordChange = text => {
+        this.props.passwordChanged(text);
+    }
+
+    render() {
+        return (
+            <Card>
+                <CardSection>
+                    <Input
+                        label="Email"
+                        placeholder="email@gmail.com"
+                        onChangeText={this.onEmailChange}
+                        value={this.props.email}
+                    />
+                </CardSection>
+                <CardSection>
+                    <Input
+                        secureTextEntry
+                        label="Password"
+                        placeholder="password"
+                        onChangeText={this.onPasswordChange}
+                        value={this.props.password}
+                    />
+                </CardSection>
+                <CardSection>
+                    <Button>
+                        Login
             </Button>
-        </CardSection>
-      </Card>
-    );
-  }
+                </CardSection>
+            </Card>
+        );
+    }
 }
 
 const mapStateToProps = state => {
     return {
-        email: state.auth.email
+        email: state.auth.email,
+        password: state.auth.password
     }
 }
 
-export default connect(mapStateToProps, { emailChanged })(LoginForm);
+export default connect(mapStateToProps, { emailChanged, passwordChanged })(LoginForm);
--- manager/src/reducers/AuthReducer.js
+++ manager/src/reducers/AuthReducer.js
@@ -1,7 +1,11 @@
-import { EMAIL_CHANGED } from '../actions/types';
+import { 
+    EMAIL_CHANGED,
+    PASSWORD_CHANGED
+} from '../actions/types';
 
 const initialState = {
-    email: ''
+    email: '',
+    password: ''
 };
 
 export default (state = initialState, action) => {
@@ -11,6 +15,11 @@ export default (state = initialState, action) => {
                 ...state,
                 email: action.payload
             };
+        case PASSWORD_CHANGED:
+            return {
+                ...state,
+                password: action.payload
+            }
         default:
             return state;
     }
