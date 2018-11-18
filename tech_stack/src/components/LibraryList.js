172ae7491a713fc272922668c79ae85ca7025313
tyobaskara <prasetyaajibaskara@gmail.com>
2018-11-18 22:12:54 +0700

    update

--- tech_stack/src/components/LibraryList.js
+++ tech_stack/src/components/LibraryList.js
@@ -24,8 +24,6 @@ class LibraryList extends Component {
 	}
 }
 
-const mapStateToProps = state => {
-	return { libraries: state.libraries };
-};
+const mapStateToProps = state => ({ libraries: state.libraries });
 
 export default connect(mapStateToProps)(LibraryList);
