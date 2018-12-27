ff512ca2a013eef0bc0c27d64984c445480a1736
tyobaskara <prasetyaajibaskara@gmail.com>
2018-12-27 23:14:05 +0700

    flatlist infinite scroll

--- react-navigation/react_nav_des18/app/screens/Contacts.js
+++ react-navigation/react_nav_des18/app/screens/Contacts.js
@@ -1,5 +1,6 @@
 import React, { PureComponent } from 'react';
-import { View, Text, FlatList } from 'react-native';
+import { FlatList } from 'react-native';
+import _ from 'lodash';
 
 import { contacts } from '../config/data';
 import colors from '../config/colors';
@@ -7,20 +8,32 @@ import colors from '../config/colors';
 import ListItem from '../components/ListItem';
 
 class Contacts extends PureComponent {
-  _renderItem = ({ item }) => (
-    <ListItem item={item} />
-  );
-
-  render() {
-    return (
-      <FlatList
-        style={{ backgroundColor: colors.backgroundColor }}
-        data={contacts}
-        renderItem={this._renderItem}
-        keyExtractor={(item) => item.email}
-      />
-    )
+  state = {
+    counter: 30
   }
+
+  onEndReached = (data) => {
+    const { counter } = this.state;
+
+    if(counter < data.length) {
+      this.setState({counter: counter + 10})
+    }
+  }
+
+	_renderItem = ({ item }) => <ListItem item={item} />;
+
+	render() {
+		return (
+			<FlatList
+				style={{ backgroundColor: colors.backgroundColor }}
+				data={_.slice(contacts,0 , this.state.counter)}
+				renderItem={this._renderItem}
+				keyExtractor={item => item.email}
+				onEndReached={() => this.onEndReached(contacts)}
+				onEndReachedThreshold={0.7}
+			/>
+		);
+	}
 }
 
 export default Contacts;
