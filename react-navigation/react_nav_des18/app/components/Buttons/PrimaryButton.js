b3e3f4d42466d17bf2b8ccba38b37f6c8f54fe9b
tyobaskara <prasetyaajibaskara@gmail.com>
2018-12-31 10:03:04 +0700

    button edit profile

---
+++ react-navigation/react_nav_des18/app/components/Buttons/PrimaryButton.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { View, TouchableOpacity, Text } from 'react-native';
+
+import styles from './styles';
+
+const PrimaryButton = ({ label, onPress }) => {
+	return (
+		<View style={styles.primaryButtonContainer}>
+			<TouchableOpacity onPress={onPress} style={styles.primaryButton}>
+				<Text style={styles.primaryButtonText}>{label}</Text>
+			</TouchableOpacity>
+		</View>
+	);
+};
+
+export default PrimaryButton;
--- react-navigation/react_nav_des18/app/screens/Personal.js
+++ react-navigation/react_nav_des18/app/screens/Personal.js
@@ -5,11 +5,17 @@ import { personal } from '../config/data';
 import { Actions, Header, Info } from '../components/UserDetails';
 import colors from '../config/colors';
 
+import { PrimaryButton } from '../components/Buttons';
+
 class Personal extends PureComponent {
 	render() {
 		return (
 			<ScrollView style={{ backgroundColor: colors.background }}>
 				<Header {...personal} />
+				<PrimaryButton 
+					label="Edit Profile"
+					onPress={() => null}
+				/>
 				<Actions {...personal} />
 				<Info {...personal} />
 			</ScrollView>
