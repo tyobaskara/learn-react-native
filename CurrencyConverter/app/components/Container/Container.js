8cea8d862d3c4d536b2365346cef1730ed945ac1
tyobaskara <prasetyaajibaskara@gmail.com>
2019-01-16 13:12:09 +0700

    Keyboard dismiss

--- CurrencyConverter/app/components/Container/Container.js
+++ CurrencyConverter/app/components/Container/Container.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { SafeAreaView } from 'react-native';
+import { SafeAreaView, View, TouchableWithoutFeedback, Keyboard } from 'react-native';
 
 import Styles from './styles';
 
 const Container = ({ children }) => (
-  <SafeAreaView style={Styles.container}>{children}</SafeAreaView>
+	<SafeAreaView style={Styles.container}>
+		<TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
+			<View style={Styles.container}>{children}</View>
+		</TouchableWithoutFeedback>
+	</SafeAreaView>
 );
 
 Container.propTypes = {
-  children: PropTypes.any,
+	children: PropTypes.any
 };
 
 export default Container;
