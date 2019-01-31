c1c898d5aeb74d1506e3b7ea76fbec4b02c693a1
Prasetya Aji Baskara <prasetyaajibaskara@Prasetyas-MacBook-Pro-2.local>
2019-01-31 11:46:18 +0700

    bar chart

--- charts/app/components/BarChart/styles.js
+++ charts/app/components/BarChart/styles.js
@@ -3,10 +3,4 @@ import EStyleSheet from 'react-native-extended-stylesheet';
 
 const { width } = Dimensions.get('window');
 
-export default EStyleSheet.create({
-  container: {
-    height: 250,
-    width: width,
-    backgroundColor: '#323232'
-  }
-});
+export default EStyleSheet.create({});
